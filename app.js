ba163bfc20ef803f3566c11a4258db0ef57872ac
MrBista <gustibisman2308@gmail.com>
2023-01-11 21:37:28 +0700

    ui bos

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
-const express = require('express')
-const app = express()
-const route = require('./routes/index')
-const session = require('express-session')
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+const route = require('./routes/index');
+const session = require('express-session');
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie:{
-    secure:false,
-    sameSite:true
-  }
-}))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      sameSite: true,
+    },
+  })
+);
 
-app.use('/', route)
+app.use('/', route);
 
 app.listen(9000, () => {
-  console.log('server listen to the port 9000')
-})
+  console.log('server listen to the port 9000');
+});

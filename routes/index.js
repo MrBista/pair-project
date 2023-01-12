d53663ff789b97540ed793efe213afa277dc5b85
fahmiMaulana1337 <fahmimaulana1337@gmail.com>
2023-01-12 11:23:59 +0700

    fiturAdminDone

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,14 @@ route.use('/user', authRoute);
 
 route.use(checkedIsLogin);
 
-route.get('/', Controller.renderHome);
+route.get('/', Controller.userHome);
 
 // middleware untuk cek role admin
 route.use(checkIsAdmin);
 route.get('/admin', Controller.homeAdmin);
+route.get('/admin/addBook', Controller.adminAddBook);
+route.post('/admin/addBook', Controller.postAdminAddbook);
+route.get('/admin/addCategory', Controller.adminAddCategory);
+route.post('/admin/addCategory', Controller.postAdminAddCategory);
 
 module.exports = route;
